eaecda5545b3bd93c44b15cfb4ae8f0224440cfd
DKBurnsBresmed <dburns@bresmed.com>
2018-12-09 19:31:18 +0000

    Setting up the project, designed to provide basic training in heemod

--- learning heemod.R
+++ learning heemod.R
@@ -1,1 +1,13 @@
+
+
 #learning heemod
+
+### this script is for playing around with heemod
+
+
+# package installation function -------------------------------------------
+
+
+
+# working through the examples --------------------------------------------
+
